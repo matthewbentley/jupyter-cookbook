f94a6f442413ca8265035b0783a5b849a0250b9c
Henry Addison <henryaddison@users.noreply.github.com>
2017-09-07 15:30:52 +0100

    bump version to accomodate README one-line

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'hja22@hja22.co.uk'
 license 'MIT'
 description 'Installs/Configures Jupyter and runs notebook as a service'
 long_description 'Installs/Configures Jupyter and creates, enables and starts a service to run a Jupyter notebook server'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 supports 'ubuntu', '= 16.04'
